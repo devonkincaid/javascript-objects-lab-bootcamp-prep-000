24c79f1a4645e0f0de35aa9384a3d5c42a9175d5
Devon Kincaid <tenaciousdk86@aol.com>
2018-02-06 22:54:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,5 +8,6 @@ function updateObjectWithKeyAndValue(obj, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  return object.key = value;
+  object.key = value;
+  return object;
 }
